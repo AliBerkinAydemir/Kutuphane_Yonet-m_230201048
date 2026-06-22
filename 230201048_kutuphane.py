ab7174c2449365d2ff9e6a663888a143723919fc
AliBerkinAydemir <aliberkinaydemir@gmail.com>
2026-06-23 00:22:59 +0300

    Kitap ekleme,  silme, listeleme fonksiyonlarını ekledim

--- 230201048_kutuphane.py
+++ 230201048_kutuphane.py
@@ -17,3 +17,43 @@ def db_baslat():
     
     db.commit()
     db.close()
+
+# *** KİTAP İŞLEMLERİ ***
+def kitap_ekle(kitap_adı, yazar, kitap_no):
+    try:
+        db = sqlite3.connect("library.db")
+        isaret = db.cursor()
+        isaret.execute("INSERT INTO books (kitap_adı, yazar, kitap_no) VALUES (?, ?, ?)", (kitap_adı, yazar, kitap_no))
+        db.commit()
+        print(f"\n[BAŞARILI] '{kitap_adı}' kitabı başarıyla eklendi :) ")
+    except sqlite3.IntegrityError:
+        print("\n[HATA] Bu KITAP_NO numarasıyla zaten bir kitap kayıtlı :( )")
+    finally:
+        db.close()
+
+def kitaplari_listele():
+    db = sqlite3.connect("library.db")
+    isaret = db.cursor()
+    isaret.execute("SELECT id, kitap_adı, yazar, kitap_no, is_available FROM books")
+    books = isaret.fetchall()
+    db.close()
+    
+    if not books:
+        print("\nKütüphanede henüz kitap yok :( )")
+        return
+    
+    print("\n*** KITAP LISTESI ***")
+    for b in books:
+        durum = "Mevcut" if b[4] == 1 else "Ödünç Verildi :)"
+        print(f"ID: {b[0]} | İsim: {b[1]} | Yazar: {b[2]} | KITAP_NO: {b[3]} | Durum: {durum}")
+
+def kitap_sil(book_id):
+    db = sqlite3.connect("library.db")
+    isaret = db.cursor()
+    isaret.execute("DELETE FROM books WHERE id = ?", (book_id,))
+    if isaret.rowcount > 0:
+        print(f"\n[BAŞARILI] ID'si {book_id} olan kitap silindi :) ")
+    else:
+        print("\n[HATA] Bu ID'ye sahip bir kitap bulunamadı :( )")
+    db.commit()
+    db.close()
