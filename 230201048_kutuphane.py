0b31647647bf72fb6bc91357ae016a33403dc634
AliBerkinAydemir <aliberkinaydemir@gmail.com>
2026-06-23 02:11:19 +0300

    Üye yönetimi,  kitap ödünç alma ve iade etme mekanizmasını yaptım

--- 230201048_kutuphane.py
+++ 230201048_kutuphane.py
@@ -57,3 +57,78 @@ def kitap_sil(book_id):
         print("\n[HATA] Bu ID'ye sahip bir kitap bulunamadı :( )")
     db.commit()
     db.close()
+
+# *** ÜYE İŞLEMLERİ ***
+def uye_ekle(name, email):
+    try:
+        db = sqlite3.connect("library.db")
+        isaret = db.cursor()
+        isaret.execute("INSERT INTO members (name, email) VALUES (?, ?)", (name, email))
+        db.commit()
+        print(f"\n[BAŞARILI] '{name}' isimli üye başarıyla kaydedildi :) ")
+    except sqlite3.IntegrityError:
+        print("\n[HATA] Bu e-posta adresiyle zaten bir üye kayıtlı!")
+    finally:
+        db.close()
+
+def uyeleri_goster():
+    db = sqlite3.connect("library.db")
+    isaret = db.cursor()
+    isaret.execute("SELECT id, name, email FROM members")
+    members = isaret.fetchall()
+    db.close()
+    
+    if not members:
+        print("\nSistemde kayıtlı üye yok :( )")
+        return
+    
+    print("\n*** UYE LISTESI ***")
+    for m in members:
+        print(f"ID: {m[0]} | İsim: {m[1]} | E-posta: {m[2]}")
+
+# *** ÖDÜNÇ ALMA & İADE İSLEMLERİ ***
+def odunc_ver(book_id, member_id):
+    db = sqlite3.connect("library.db")
+    isaret = db.cursor()
+    
+    # Kitap kontrolü
+    isaret.execute("SELECT is_available FROM books WHERE id = ?", (book_id,))
+    res = isaret.fetchone()
+    
+    if res is None:
+        print("\n[HATA] Kitap bulunamadı :( )")
+        db.close()
+        return
+    if res[0] == 0:
+        print("\n[HATA] Bu kitap zaten ödünç verilmiş :( )")
+        db.close()
+        return
+        
+    # İşlemi kaydet
+    tarih = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    isaret.execute("INSERT INTO transactions (book_id, member_id, borrow_date) VALUES (?, ?, ?)", (book_id, member_id, tarih))
+    isaret.execute("UPDATE books SET is_available = 0 WHERE id = ?", (book_id,))
+    
+    db.commit()
+    db.close()
+    print("\n[BAŞARILI] Kitap başarıyla ödünç verildi :)")
+
+def iade_al(book_id):
+    db = sqlite3.connect("library.db")
+    isaret = db.cursor()
+    
+    isaret.execute("SELECT id FROM transactions WHERE book_id = ? AND return_date IS NULL", (book_id,))
+    t_id = isaret.fetchone()
+    
+    if t_id is None:
+        print("\n[HATA] Bu kitaba ait aktif bir ödünç alma işlemi bulunamadı :( )")
+        db.close()
+        return
+        
+    tarih = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    isaret.execute("UPDATE transactions SET return_date = ? WHERE id = ?", (tarih, t_id[0]))
+    isaret.execute("UPDATE books SET is_available = 1 WHERE id = ?", (book_id,))
+    
+    db.commit()
+    db.close()
+    print("\n[BAŞARILI] Kitap başarıyla iade alındı :)")
